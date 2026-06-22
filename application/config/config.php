634782fe81c4d5b5c61fc02704e0f9c9cc8bddc2
Tara Amelia <hyicaramel@gmail.com>
2026-06-22 21:48:47 +0700

    U

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/poktan_kopi_merged/';
+$config['base_url'] = 'http://localhost/poktan_kopi/';
 
 /*
 |--------------------------------------------------------------------------
