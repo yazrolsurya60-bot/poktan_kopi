9d2d5337dabefc15232dc5b4041ec12cc77e17cc
Gyll346 <ragyl03alfari@gmail.com>
2026-07-02 19:58:07 +0700

    Perbaikan pada Beranda

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -4,6 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Beranda extends CI_Controller {
 
     public function index() {
-        $this->load->view('v_beranda');
+        $this->load->database();
+
+        $data['total_petani'] = $this->db->where('role', 'Petani')->count_all_results('tb_user');
+        $data['total_mitra'] = $this->db->count_all_results('tb_mitra');
+        $data['total_produk'] = $this->db->count_all_results('tb_produk');
+
+        $this->load->view('v_beranda', $data);
     }
 }
